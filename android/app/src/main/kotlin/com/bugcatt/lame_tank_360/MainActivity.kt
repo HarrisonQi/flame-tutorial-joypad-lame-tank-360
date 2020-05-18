35430da0867e5695f04e9b3dae0a48256cefac33
阿航 <harrisonqi@foxmail.com>
2020-05-18 16:46:22 +0800

    :tada: 首次提交, 初始化项目!

---
+++ android/app/src/main/kotlin/com/bugcatt/lame_tank_360/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bugcatt.lame_tank_360
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
